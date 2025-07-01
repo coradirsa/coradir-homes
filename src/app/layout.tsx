ade6031d924d8b0cb73330d3c6156b46846a0f1a
ValentinGarro <vgarro@coradir.com.ar>
2025-07-01 15:52:27 -0300

    agregando etiquetas script de nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import type { Metadata } from "next"; 
 import "./globals.css";
 import Header from "./components/header/header"; 
 import Footer from "./components/footer/footer";
 import Bot from "./components/bot"; 
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Coradir Homes",
@@ -18,9 +20,9 @@ export default function RootLayout({
     <html lang="es">
       <head>
         {/* Google Tag Manager */}
-        <script dangerouslySetInnerHTML={{ __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-PBZQ65VZ');`}} />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0FW1131XF8"></script>
-        <script dangerouslySetInnerHTML={{ __html: `
+        <Script id="gtm-script" strategy="beforeInteractive"  dangerouslySetInnerHTML={{ __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-PBZQ65VZ');`}} />
+        <Script  strategy="beforeInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-0FW1131XF8"/>
+        <Script id="gtm-script-2" strategy="beforeInteractive" async dangerouslySetInnerHTML={{ __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
