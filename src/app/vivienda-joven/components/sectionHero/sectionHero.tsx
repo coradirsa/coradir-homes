ecc6661c672defbc742ccf709dd7ed856830f048
ValentinGarro <vgarro@coradir.com.ar>
2025-08-07 11:11:43 -0300

    arreglando tamaños y texto en hero section vivienda joven

--- src/app/vivienda-joven/components/sectionHero/sectionHero.tsx
+++ src/app/vivienda-joven/components/sectionHero/sectionHero.tsx
@@ -5,13 +5,13 @@ export default function SectionHero() {
     return (
         <section className={`relative w-full flex flex-col items-center justify-center gap-8 md:gap-14`} style={{ height: `${H}vh` }}>
         <h1
-         className="text-3xl md:text-5xl xl:text-8xl text-center font-playfair text-white"
+         className="text-5xl xl:text-8xl text-center font-playfair text-white"
          style={{
             textShadow: '6px 3px 4px rgba(0,0,0,0.5)',
          }}
-        > Reservá tu futuro hogar<br/>desde $250 USD mensuales</h1>
-        <p className="text-center text-white text-lg xl:text-3xl"> Departamentos inteligentes en Juana Koslay · <br/> Entrega proyectada · Cupos limitados</p>
-        <ButtonContact href="#formulario" className="uppercase bg-white text-blue hover:bg-white/40 hover:text-white hover:border-white border-1" label=" RESERVÁ AHORA EN PREVENTA"/>
+        > Proyectos en Pozo<br/>con Valor Futuro</h1>
+        <p className="text-center text-white max-w-[90%] md:max-w-full text-lg xl:text-3xl"> Departamentos inteligentes en Juana Koslay · <br/> Entrega proyectada · Cupos limitados</p>
+        <ButtonContact href="#formulario" className="text-xl md:text-2xl uppercase bg-white text-blue hover:bg-white/40 hover:text-white hover:border-white border-1" label=" RESERVÁ AHORA"/>
         <div className={`absolute top-0 left-0 w-full -z-1 bg-[rgba(26,52,85,0.5)]`} style={{ height: `${H}vh` }}></div>
         <Image 
             src="/img/vivienda-joven/hero.png"
