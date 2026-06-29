3c20597d47f8b7e15d50c9b84fdb541a64e07070
KaruX <khxzsalini@gmail.com>
2026-06-29 09:02:27 -0300

    Fix build de maqueta de locales

--- src/app/locales-comerciales-maqueta/page.tsx
+++ src/app/locales-comerciales-maqueta/page.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import ReCaptcha from "../components/reCaptcha";
-import ProjectForm from "../components/projectForm";
-import { SITE_DATA } from "../locales-comerciales/data";
-import FeaturesBar from "../locales-comerciales/components/FeaturesBar";
-import EnergyBackup from "../locales-comerciales/components/EnergyBackup";
-import {
-  Benefits,
-  CommercialOptions,
-  Hero,
-  Location,
-  UseCases,
-} from "./components/LocalesMockup";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Maqueta Locales Comerciales | CORADIR Homes",
@@ -20,28 +9,6 @@ export const metadata: Metadata = {
   },
 };
 
-export const revalidate = 3600;
-
 export default function LocalesComercialesMaquetaPage() {
-  return (
-    <ReCaptcha>
-      <>
-        <Hero data={SITE_DATA} />
-        <FeaturesBar data={SITE_DATA} />
-        <CommercialOptions />
-        <Location data={SITE_DATA} />
-        <EnergyBackup data={SITE_DATA} />
-        <Benefits />
-        <UseCases />
-        <ProjectForm
-          id="contact"
-          interest="locales-comerciales"
-          heading={SITE_DATA.contact.title}
-          subtitle={SITE_DATA.contact.subtitle}
-          backgroundImage={SITE_DATA.contact.bgImage}
-          transactionTypes={["comprar", "alquilar", "leasing"]}
-        />
-      </>
-    </ReCaptcha>
-  );
+  redirect("/locales-comerciales");
 }
